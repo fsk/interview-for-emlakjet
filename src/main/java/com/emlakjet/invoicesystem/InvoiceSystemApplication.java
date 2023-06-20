94ef3d69ab06396475edffdd2d54041ff8df7d72
0xfsk <furkankulaksiz68@gmail.com>
2023-06-20 21:45:14 +0300

    Initial commit

---
+++ src/main/java/com/emlakjet/invoicesystem/InvoiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.emlakjet.invoicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emlakjet/invoicesystem/InvoicesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emlakjet.invoicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
