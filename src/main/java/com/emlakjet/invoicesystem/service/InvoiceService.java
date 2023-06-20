4bd106e9ed1aedd236a28925183fdfa58695a7bf
0xfsk <furkankulaksiz68@gmail.com>
2023-06-21 01:40:41 +0300

    Invoice service icerisinde DB'deki total amounta bakilarak kontrol saglandi.

--- src/main/java/com/emlakjet/invoicesystem/service/InvoiceService.java
+++ src/main/java/com/emlakjet/invoicesystem/service/InvoiceService.java
@@ -1,13 +1,13 @@
 package com.emlakjet.invoicesystem.service;
 
-
-import com.emlakjet.invoicesystem.controller.InvoiceController;
 import com.emlakjet.invoicesystem.entity.AccountingSpecialist;
 import com.emlakjet.invoicesystem.entity.Invoice;
 import com.emlakjet.invoicesystem.repository.AccountingSpecialistRepository;
 import com.emlakjet.invoicesystem.repository.InvoiceRepository;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -19,6 +19,11 @@ public class InvoiceService {
 
     private final InvoiceRepository invoiceRepository;
     private final AccountingSpecialistRepository accountingSpecialistRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(InvoiceService.class);
+
+    @Value("${emlakjet.amount.value}")
+    private Integer amount;
 
 
     public Invoice createNewInvoice(Invoice invoice) throws IllegalAccessException {
@@ -32,6 +37,15 @@ public class InvoiceService {
 
         if (!accountingSpecialist.isPresent()) {
             throw new IllegalAccessException("Accounting Specialist Not Found.!");
+        }
+
+        int amountValue = accountingSpecialist.get().getInvoices().stream().mapToInt(Invoice::getAmount).sum();
+
+        logger.info("AmountValue from DB : {} ", amountValue);
+
+
+        if (amountValue > this.amount) {
+            throw new IllegalAccessException("Amount is should be less than total amount in Database");
         }
 
         Invoice savedInvoice = Invoice
